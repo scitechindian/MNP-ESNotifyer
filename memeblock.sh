d91f2ea50906256087aed1df3621e3c6f39f385c
Yoshi <jason@htmllabs.com>
2017-09-13 17:39:22 -0400

    Added Coins

---
+++ memeblock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/php /root/notify/esnotify/blockFound.php "meme" "$@" >> meme.blockfound.log
---
+++ xcxtblock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#curl "http://crm.masternodes.pro/coin/block" -d "$@"
+/usr/bin/php /root/notify/esnotify/blockFound.php "xcxt" "$@" >> xcxt.blockfound.log
